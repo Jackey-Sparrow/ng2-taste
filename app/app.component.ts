83066ba925cf988e7bc3a2dfcd6ebd079e238a62
Jackey.Sparrow <1109952026@qq.com>
2015-12-23 20:57:12 +0800

    fix error

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,16 +6,25 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-         <h2>{{myHero}} details!</h2>`
+         <h2>{{myHero.name}} details!</h2>`
 })
+
+interface Hero {
+    id:number;
+    name:string;
+}
+
 export class AppComponent {
 
     title:string;
-    myHero:string;
+    public myHero:Hero = {
+        id: 1,
+        name: 'Jackey'
+    }
 
     constructor() {
         this.title = 'Tour of Heroes';
-        this.myHero = 'Jackey';
+
     }
 
 }
